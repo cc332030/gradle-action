a9b9a95dc3b1e34ce9fd5f0c63b2ae6c209c86b9
c332030 <c332030@gmail.com>
2022-07-11 22:24:12 +0800

    feat: gradle action

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,8 +8,10 @@ curl_sh(){
   sh -c "curl -sL https://github.com/cc332030$1 | sh -s -- $parameter"
 }
 
-curl_sh /linux/raw/master/script/init-ssh/init-ssh-pre.sh
-curl_sh /linux/raw/master/script/init-ssh/init-ssh.sh
+if [ "$SSH_PRIVATE_KEY" ]; then
+  curl_sh /linux/raw/master/script/init-ssh/init-ssh-pre.sh
+  curl_sh /linux/raw/master/script/init-ssh/init-ssh.sh
+fi
 
 if [ "$GPG_PRIVATE_KEY" ]; then
   curl_sh /linux/raw/master/script/init-gpg/init-gpg.sh
@@ -17,7 +19,4 @@ fi
 
 curl_sh /gradle/raw/master/script/init-gradle/init-gradle.sh
 
-if [ -n "$COMMAND" ]
-then
-  ${COMMAND} || true
-fi
+$1
