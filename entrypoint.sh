d998cb446573049d01ae3bc41339af7bc70dd98c
c332030 <c332030@gmail.com>
2022-07-12 22:18:11 +0800

    feat: add ssh

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,6 +16,11 @@ curl_sh /gradle/raw/master/script/init-gradle/init-gradle.sh
 
 $1
 
+if [ "$SSH_PRIVATE_KEY" ]; then
+  curl_sh /linux/raw/master/script/init-ssh/init-ssh-pre.sh
+  curl_sh /linux/raw/master/script/init-ssh/init-ssh.sh
+fi
+
 if [ "$HOST" ] && [ "$PORT" ]; then
   curl_sh /linux/raw/master/script/deploy/deploy.sh
 fi
