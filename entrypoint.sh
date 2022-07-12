503131e3d05e6cd72582990d2aa8d334e12ffd59
c332030 <c332030@gmail.com>
2022-07-12 21:12:36 +0800

    feat: remove ssh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,11 +8,6 @@ curl_sh(){
   sh -c "curl -sL https://github.com/cc332030$1 | sh -s -- $parameter"
 }
 
-if [ "$SSH_PRIVATE_KEY" ]; then
-  curl_sh /linux/raw/master/script/init-ssh/init-ssh-pre.sh
-  curl_sh /linux/raw/master/script/init-ssh/init-ssh.sh
-fi
-
 if [ "$GPG_PRIVATE_KEY" ]; then
   curl_sh /linux/raw/master/script/init-gpg/init-gpg.sh
 fi
